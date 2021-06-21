65e138b876200b2680fd2068f9c5d346ac242505
Rafael Junqueira Santos <delnuss@gmail.com>
2021-06-21 19:37:11 -0300

    added texts to the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="page-home">
     <Header />
+    <div class="text-center mt-5">
+      <p class="h1">Select your view on the menu</p>
+      <p class="h4">The charts are updated every minute.</p>
+    </div>
   </div>
 </template>
